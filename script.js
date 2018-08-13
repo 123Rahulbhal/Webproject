5b7d73b1f62a66dc7c46d9801f605683c82972e9
RahulBhai <rahul.09baba@gmail.com>
2018-08-13 12:56:29 +0530

    Made some more changes in all the files

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let ip = ['192.168.13.1','192.168.13.2','192.168.13.3','192.168.13.4','192.168.13.5','192.168.13.6','192.168.13.7','192.168.13.8','192.168.13.9','192.168.13.10'];
 for(index = 0; index < ip.length; index++){
     if(ip[index] === '192.168.13.7'){
-        let value = prompt('Enter New IP Address: ')
-        ip[index] = value;
+        let value = prompt('Enter New IP Address: ');
         console.log('Rahul');
     }
 }
